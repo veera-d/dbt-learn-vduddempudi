d83ee2e3a25384bfa7ebd1ebc8de1e6d9c877a58
Veera Duddempudi <veera.duddempudi@usaa.com>
2020-09-29 20:55:48 +0000

    class execersies

---
+++ models/CaseStudy/stg_event_orders.sql
@@ -0,0 +1,5 @@
+select 
+ID as event_id,
+STATUS as status,
+REFUND_AMOUNT as refund_amount
+from raw.ticket_tailor.orders
--- models/stg_payment.sql
+++ models/stg_payment.sql
@@ -2,7 +2,6 @@ select
 id as customer_id, 
 orderid as order_id,
 paymentmethod as payment_method,
-status as status,
 amount/100 as amount
-from raw.stripe.payment
+from {{ source('stripe', 'payment') }}
 where status ='success'
